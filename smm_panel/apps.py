4ac73c21706e8eae22114bc088e9f735f0ccdd64
chistev <stephenowabie@gmail.com>
2025-07-31 08:46:43 +0100

    inital commit

---
+++ smm_panel/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SmmPanelConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'smm_panel'
---
+++ smm_panel/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'smm_panel/index.html')
