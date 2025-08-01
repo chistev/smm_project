c2aa989bbc95e0ff5b70f4151d4d3f582bc7049a
chistev <stephenowabie@gmail.com>
2025-08-01 11:04:41 +0100

    base and signup html added

--- smm_panel/views.py
+++ smm_panel/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'smm_panel/index.html')
+
+def signup(request):
+    return render(request, 'smm_panel/signup.html')
