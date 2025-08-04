342eb1d11ece931a9582e44209e75ff71e320e79
chistev <stephenowabie@gmail.com>
2025-08-04 10:13:52 +0100

    forgot password page

--- smm_panel/views.py
+++ smm_panel/views.py
@@ -8,3 +8,10 @@ def signup(request):
 
 def login(request):
     return render(request, 'smm_panel/login.html')
+
+def forgot_password(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        # TODO: Add password reset logic here (e.g., send reset link)
+        return render(request, 'smm_panel/forgot_password.html', {'message': 'If the email exists, a reset link has been sent.'})
+    return render(request, 'smm_panel/forgot_password.html')
